766b54ca81ec64df82b0378ad94977a6255c6a22
杨朋亮 <yangpengliang@yangpengliangdeMacBook-Pro.local>
2014-12-30 14:36:11 +0800

    interrect

--- ViewController.m
+++ ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 #import "MarqueeLabel.h"
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+#define SETCOLOR(RED,GREEN,BLUE) [UIColor colorWithRed:RED/255 green:GREEN/255 blue:BLUE/255 alpha:1.0]
+
+//RGB颜色
+#define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
+//RGB颜色和不透明度
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f \
+alpha:(a)]
+
 
 @interface ViewController ()
 
@@ -20,13 +30,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view setBackgroundColor:[UIColor whiteColor]];
-    
-    self.autoScrollLabel.enabled = YES;
-    [self.view addSubview:self.autoScrollLabel];
+    [self.view setBackgroundColor:UIColorFromRGB(0x339900)];
     
     self.autoScrollLabel.marqueeType = MLContinuous;
     [self.autoScrollLabel setFont:[UIFont systemFontOfSize:300]];
+    [self.autoScrollLabel setTextColor:UIColorFromRGB(0xFFFFFF)];
     self.autoScrollLabel.scrollDuration = 10.0f;
     self.autoScrollLabel.fadeLength = 10.0f;
     self.autoScrollLabel.trailingBuffer = 30.0f;
@@ -38,7 +46,18 @@
     tapRecognizer.numberOfTouchesRequired = 1;
     [self.autoScrollLabel addGestureRecognizer:tapRecognizer];
     
-    [self.autoScrollLabel restartLabel];
+    UISwipeGestureRecognizer *swipeUp = [[UISwipeGestureRecognizer alloc]
+                                           initWithTarget:self
+                                           action:@selector(handleSwipeUp)];
+    swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
+    [self.view addGestureRecognizer:swipeUp];
+    
+    UISwipeGestureRecognizer *swipeDown = [[UISwipeGestureRecognizer alloc]
+                                            initWithTarget:self
+                                            action:@selector(handleSwipeDown)];
+    swipeDown.direction = UISwipeGestureRecognizerDirectionDown;
+    [self.view addGestureRecognizer:swipeDown];
+
     
 }
 - (void)pauseTap:(UITapGestureRecognizer *)recognizer {
@@ -52,6 +71,17 @@
         }
     }
 }
+
+-(void) handleSwipeUp{
+    
+    
+}
+
+-(void) handleSwipeDown{
+  
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
